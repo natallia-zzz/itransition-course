dbfa2afb115e8e7a951756283512469d455e9a0a
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2021-07-18 22:27:45 +0300

    console parsing

--- lcs.js
+++ lcs.js
@@ -1,25 +1,29 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	var stringToSplit =  "node anode nodeb cnoded"
-	var arrayOfStrings = stringToSplit.split(" ");
-	var lengthOfArr = arrayOfStrings.length;
-	var firstWord = arrayOfStrings[0];
-	var result = "";
-	var temp = "";
-	var k = 1;
-	for(var i = 0; i < firstWord.length; i++){
-		for (var j = i + 1; j < lengthOfArr +1 ; j++) {
-			temp = firstWord.slice(i,j);
-			for(k = 1; k < lengthOfArr ; k++){
-				if(!arrayOfStrings[k].includes(temp)){
-					break;
-				}	
-			}
-			if(k == lengthOfArr && result.length < temp.length){
-				result = temp;
+	if(process.argv.length > 2){
+		var arrayOfStrings = process.argv.slice(2);
+		var lengthOfArr = arrayOfStrings.length;
+		var firstWord = arrayOfStrings[0];
+		var result = "";
+		var temp = "";
+		var k = 1;
+		for(var i = 0; i < firstWord.length; i++){
+			for (var j = i + 1; j < firstWord.length + 1 ; j++) {
+				temp = firstWord.slice(i,j);
+				for(k = 1; k < lengthOfArr ; k++){
+					if(!arrayOfStrings[k].includes(temp)){
+						break;
+					}	
+				}
+				if(k == lengthOfArr && result.length < temp.length){
+					result = temp;
+				}
 			}
 		}
+		console.log(result);
 	}
-	console.log(result);
+	else{
+		console.log("nothing entered");
+	}
 }).listen(8080);
