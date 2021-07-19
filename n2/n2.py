50ddf4044c4991da6a32c07215914cf226374ee5
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2021-07-19 12:41:31 +0300

    added hash256

--- n2/n2.py
+++ n2/n2.py
@@ -1,3 +1,12 @@
+import hashlib
+
+def file_contents_hash(path):
+	file = open(path)
+	line = file.read()
+	file.close()
+	bline = str.encode(line)
+	return hashlib.sha256(bline).hexdigest()
+
 def main():
 	mypath = "/Users/natalliazzz/itransition course/n2/files"
 	from os import listdir
@@ -5,6 +14,7 @@ def main():
 	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 	for i in onlyfiles:
 		print(i)
+		print(file_contents_hash(join(mypath, f)))
 
 
 if __name__ == '__main__':
