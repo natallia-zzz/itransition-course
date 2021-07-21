7db9faba3bf0a8ebe2db970ed97e1c364cd7d51f
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2021-07-21 12:15:47 +0300

    compressed file

--- n1/lcs.js
+++ n1/lcs.js
@@ -1,2 +1,1 @@
-if(process.argv.length>2){var a=process.argv.slice(2),l=a.length,f=a[0],r="",t="",k=1;for(var i=0;i<f.length;i++){for(var j=i+1;j<f.length+1;j++){t=f.slice(i,j);
-for(k=1;k<l;k++){if(!a[k].includes(t)){break;}}if(k==l&&r.length<t.length){r=t;}}}console.log(r);}else{console.log("nothing entered");}
+if(process.argv.length>2){var a=process.argv.slice(2),l=a.length,g=a[0].length,r="",t;for(var i=0;i<g;i++){for(var j=i+1;j<g+1;j++){t=a[0].slice(i,j);if(a.every(e=>e.includes(t))&&r.length<j-i){r=t;}}}console.log(r);}
